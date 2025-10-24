452d491958b3c8fc38da5f9a2ab73b3548257126
Yash Upadhyay <91269999+YashUpadhyay0456@users.noreply.github.com>
2025-10-24 13:41:54 +0000

    basics donegit add .

---
+++ main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+func main (){
+	//fmt.Println("Hello, World!");
+	fmt.Print("go"+"lang\n");
+	fmt.Println("1+1 =", 1+1, "\n");
+	fmt.Println("0.7/0.3=", 0.7/0.3);
+	fmt.Println(true && false);
+
+	//variables
+	var a = "Yash";
+	fmt.Println("Hello,", a);
+	var b int = 42 ;
+	fmt.Println(b);
+	c := 3.14;
+	c = 34.56
+	fmt.Println(c);
+
+	//Constants
+	const pi = 3.14159
+	fmt.Println("Pi =", pi);
+
+	//loops 
+	// while
+	i:= 1;
+	for i< 12 {
+		fmt.Println(i);
+		i++;
+	}
+	
+	// for 
+	for a := 0 ; a < 4 ; a++{
+		fmt.Println("a =", a);
+	} 
+
+
+}			
