6d28fffa8875c2bdf8ca09fd7edb3b1f4ad50e93
Yash Upadhyay <yashuchanu@gmail.com>
2025-10-25 19:57:21 +0530

    learned arrays!

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -22,13 +22,12 @@ func main() {
 	fmt.Println(bools)
 
 	var names [3]string
-	names[0] = "Yash";	
-	names[1] = "jayesh";
-	fmt.Println(names);	
-	var empty [12]string;
-	fmt.Println(empty);
-	delarr :=[5] int{1,2,3,4,5};
-	fmt.Print(delarr);
-
+	names[0] = "Yash"
+	names[1] = "jayesh"
+	fmt.Println(names)
+	var empty [12]string
+	fmt.Println(empty)
+	delarr := [5]int{1, 2, 3, 4, 5}
+	fmt.Print(delarr)
 
 }
