baba3ffe676f1dbf2714bcfe3a732aadfb364d17
Yash Upadhyay <yashuchanu@gmail.com>
2025-10-25 19:53:28 +0530

    learned arrays

---
+++ arrays/arrays.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi lets learn arrays in Go !")
+
+	var nums [4]int
+	for i := range 4 {
+		nums[i] = 4 * i
+	}
+	for i := range 4 {
+		fmt.Println(nums[i])
+	}
+
+	fmt.Println("nums entire value: ", nums)
+
+	var bools [5]bool
+	for i := range 5 {
+		bools[i] = i&1 == 0
+	}
+	fmt.Println(bools)
+
+	var names [3]string
+	names[0] = "Yash";	
+	names[1] = "jayesh";
+	fmt.Println(names);	
+	var empty [12]string;
+	fmt.Println(empty);
+	delarr :=[5] int{1,2,3,4,5};
+	fmt.Print(delarr);
+
+
+}
--- main/main.go
+++ main/main.go
@@ -4,99 +4,99 @@ import (
 	"fmt"
 	"time"
 )
-func main (){
+
+func main() {
 	//fmt.Println("Hello, World!");
-	fmt.Print("go"+"lang\n");
-	fmt.Println("1+1 =", 1+1, "\n");
-	fmt.Println("0.7/0.3=", 0.7/0.3);
-	fmt.Println(true && false);
+	fmt.Print("go" + "lang\n")
+	fmt.Println("1+1 =", 1+1, "\n")
+	fmt.Println("0.7/0.3=", 0.7/0.3)
+	fmt.Println(true && false)
 
 	//variables
-	var a = "Yash";
-	fmt.Println("Hello,", a);
-	var b int = 42 ;
-	fmt.Println(b);
-	c := 3.14;
+	var a = "Yash"
+	fmt.Println("Hello,", a)
+	var b int = 42
+	fmt.Println(b)
+	c := 3.14
 	c = 34.56
-	fmt.Println(c);
+	fmt.Println(c)
 
 	//Constants
 	const pi = 3.14159
-	fmt.Println("Pi =", pi);
+	fmt.Println("Pi =", pi)
 
-	//loops 
+	//loops
 	// while
-	i:= 1;
-	for i< 12 {
-		fmt.Println(i);
-		i++;
-	}
-	
-	// for 
-	for a := 0 ; a < 4 ; a++{
-		fmt.Println("a =", a);
-	} 
-
-	for a := 10 ; a>0 ; a--{
-		if(a == 2){
-			break;
-		}
-		if (a == 5) {
-			continue;
-		}
-		fmt.Println(a);
+	i := 1
+	for i < 12 {
+		fmt.Println(i)
+		i++
 	}
 
-	//for range 
-	fmt.Println("For Range:");
-	for  r := range 10{
-		fmt.Println(r);
+	// for
+	for a := 0; a < 4; a++ {
+		fmt.Println("a =", a)
 	}
-	for index , value := range  []string{"Go", "is", "awesome"} {
-		fmt.Println("index:", index, "value:", value);
+
+	for a := 10; a > 0; a-- {
+		if a == 2 {
+			break
+		}
+		if a == 5 {
+			continue
+		}
+		fmt.Println(a)
+	}
+
+	//for range
+	fmt.Println("For Range:")
+	for r := range 10 {
+		fmt.Println(r)
+	}
+	for index, value := range []string{"Go", "is", "awesome"} {
+		fmt.Println("index:", index, "value:", value)
 	}
 
 	//switch case
 	// no need to put break statement
-	fmt.Println("Switch Case:");
+	fmt.Println("Switch Case:")
 	data := 2
 	switch data {
 	case 1:
-		fmt.Println("Data is 1");
+		fmt.Println("Data is 1")
 	case 2:
-		fmt.Println("Data is 2");
+		fmt.Println("Data is 2")
 	case 3:
-		fmt.Println("Data is 3");
+		fmt.Println("Data is 3")
 	default:
-		fmt.Println("Data is unknown");
+		fmt.Println("Data is unknown")
 	}
 
 	// there is no ternary operator in go
 
-	// multiple condition switch 
-	switch time.Now().Weekday(){
+	// multiple condition switch
+	switch time.Now().Weekday() {
 	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend");
+		fmt.Println("It's the weekend")
 	default:
-		fmt.Println("It's a weekday");	
+		fmt.Println("It's a weekday")
 	}
-	fmt.Println(time.Now());
+	fmt.Println(time.Now())
 
 	// type switch
-	whoAmI := func (i interface{})  {
-		switch t :=i.(type) {
+	whoAmI := func(i interface{}) {
+		switch t := i.(type) {
 		case int:
-			fmt.Println("I'm an int");
+			fmt.Println("I'm an int")
 		case string:
-			fmt.Println("I'm a string");
+			fmt.Println("I'm a string")
 		default:
-			fmt.Printf("Don't know type %T\n", t);
+			fmt.Printf("Don't know type %T\n", t)
 		}
 	}
 
-	whoAmI(42);
-	whoAmI("hello");
-	whoAmI(3.14);	
+	whoAmI(42)
+	whoAmI("hello")
+	whoAmI(3.14)
 
-}			
-	
+}
